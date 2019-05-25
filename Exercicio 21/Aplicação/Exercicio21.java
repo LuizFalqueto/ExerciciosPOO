47f2b6c7a73b3d963c6da74aba54f4ff5e55252c
LuizFalqueto <lgfalqueto@hotmail.com>
2019-05-25 18:18:23 -0300

    Exercicio 21

---
+++ Exercicio 21/Aplicação/Exercicio21.java
@@ -0,0 +1,15 @@
+package Aplicação;
+
+/**
+ *
+ * @author LuizGustavo
+ */
+import Matematica.ConstantesMatematicas;
+public class Exercicio21 {
+    public static void main(String[] args) {
+        double constante;
+        constante = ConstantesMatematicas.RAIZDE6;
+	System.out.println(constante);
+    }
+    
+}
